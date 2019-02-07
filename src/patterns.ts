e905594f1fef6be832fc77078e3525863ff4f76b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-07 10:25:06 -0800

    mii

--- src/patterns.ts
+++ src/patterns.ts
@@ -13,7 +13,7 @@ const metadata: Metadata = {
     description: post,
     formattedName: 'Tsraxcfaubdj',
     mostRecentPublish: process.env.PUBLISH_DATE || '2018-10-31T07:00:00.000Z',
-    musicalIdeaIllustrated: '',
+    musicalIdeaIllustrated: 'un-pump of the Blumeyer comma (11*13*17)/19',
     originalPublish: '2018-10-31T07:00:00.000Z',
 }
 
