ac6b070da8e11860e2bedc66792dd952a1ea7643
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-08 14:36:22 -0800

    1.0.16: Hz and Ms

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,16 @@
 // tslint:disable no-magic-numbers
 
 import {
-    Frequency,
-    Milliseconds,
+    Hz,
+    Ms,
     SCIENTIFIC_PITCHES,
     ScientificPitchNoteName,
     ScientificPitchOctaveNumber,
     to,
 } from '@musical-patterns/utilities'
 
-const TSRAXCFAUBDJ_INITIAL_BASE_DURATION: Milliseconds = to.Milliseconds(2400)
-const TSRAXCFAUBDJ_INITIAL_BASE_FREQUENCY: Frequency =
+const TSRAXCFAUBDJ_INITIAL_BASE_DURATION: Ms = to.Ms(2400)
+const TSRAXCFAUBDJ_INITIAL_BASE_FREQUENCY: Hz =
     SCIENTIFIC_PITCHES[ ScientificPitchNoteName.C ][ ScientificPitchOctaveNumber._0 ]
 
 export {
--- src/materials/scales.ts
+++ src/materials/scales.ts
@@ -15,19 +15,19 @@ const buildScales: BuildScalesFunction =
 
         const gainScale: Scale = nonScale
         const durationsScale: Scale = {
-            scalar: to.Scalar(from.Milliseconds(spec[ StandardSpecProperties.BASE_DURATION ] || to.Milliseconds(1))),
+            scalar: to.Scalar(from.Ms(spec[ StandardSpecProperties.BASE_DURATION ] || to.Ms(1))),
             scalars: flatDurationsScale.scalars,
             translation: spec[ StandardSpecProperties.DURATION_TRANSLATION ] || NO_TRANSLATION,
         }
         const yerScalars: Scalar[] = generateOctaveRepeatingScalars(buildYerScalars())
         const yerPitchesScale: Scale = {
-            scalar: to.Scalar(from.Frequency(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Frequency(1))),
+            scalar: to.Scalar(from.Hz(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Hz(1))),
             scalars: yerScalars,
             translation: spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || NO_TRANSLATION,
         }
         const yerExceptionScalars: Scalar[] = generateOctaveRepeatingScalars(buildYerExceptionScalars())
         const yerExceptionPitchesScale: Scale = {
-            scalar: to.Scalar(from.Frequency(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Frequency(1))),
+            scalar: to.Scalar(from.Hz(spec[ StandardSpecProperties.BASE_FREQUENCY ] || to.Hz(1))),
             scalars: yerExceptionScalars,
             translation: spec[ StandardSpecProperties.FREQUENCY_TRANSLATION ] || NO_TRANSLATION,
         }
