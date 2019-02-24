a3d9fdc0d8a18da3968356afc51deac8370f58c3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-24 13:24:01 -0800

    1.0.38: use filter helpers

--- test/src/materials/wholes/helpers.ts
+++ test/src/materials/wholes/helpers.ts
@@ -1,12 +1,23 @@
 import { PitchDurationScale } from '@musical-patterns/pattern'
-import { apply, ContourElement, ContourWhole, forEach, numericSort, Ordinal, to } from '@musical-patterns/utilities'
+import {
+    apply,
+    ContourElement,
+    ContourWhole, filter,
+    forEach,
+    numericSort,
+    Ordinal,
+    to,
+    uniqueFilter,
+} from '@musical-patterns/utilities'
 import { buildBassContourWhole, REST, YER_PITCH_CLASS_COUNT } from '../../../../src/indexForTest'
 import { INDEX_OF_PITCH_WITHIN_CONTOUR_ELEMENT } from './constants'
 
 const getBassPitchClassIndexSet: () => number[] =
     (): number[] =>
-        getPitchClassIndices(buildBassContourWhole())
-            .filter((value: number, index: number, self: number[]) => self.indexOf(value) === index)
+        filter(
+            getPitchClassIndices(buildBassContourWhole()),
+            uniqueFilter,
+        )
             .filter((value: number) => value !== REST)
             .sort(numericSort)
 
