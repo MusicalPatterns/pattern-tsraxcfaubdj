800c249aa898be6c2f5ad99f74b3b3726d82d733
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-28 15:48:08 -0800

    1.0.50: include pattern version

--- src/patterns.ts
+++ src/patterns.ts
@@ -15,6 +15,7 @@ const metadata: Metadata = {
     mostRecentPublish: process.env.PUBLISH_DATE || '2018-10-31T07:00:00.000Z',
     musicalIdeaIllustrated: 'un-pump of the Blumeyer comma (11*13*17)/19',
     originalPublish: '2018-10-31T07:00:00.000Z',
+    version: process.env.PATTERN_VERSION || 'unknown',
 }
 
 const pattern: Pattern<StandardSpec> = {
