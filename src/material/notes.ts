920088dbfd8eb59ad0fb6a323723715a066bc2a6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-03 17:26:57 -0800

    1.0.53: how did i miss that opportunity when i cleaned that part up recently

--- src/material/notes.ts
+++ src/material/notes.ts
@@ -1,12 +1,17 @@
 import { NoteSpec } from '@musical-patterns/compiler'
-import { PitchDurationScale, SILENT, STANDARD_DURATIONS_SCALE_INDEX } from '@musical-patterns/pattern'
+import {
+    PitchDurationScale,
+    SILENT,
+    STANDARD_DURATIONS_SCALE_INDEX,
+    STANDARD_PITCH_INDEX_INDICATING_REST,
+} from '@musical-patterns/pattern'
 import { ContourElement, from, Scalar, to } from '@musical-patterns/utilities'
 
 const buildNoteSpec: (contourElement: ContourElement<PitchDurationScale>) => NoteSpec =
     (contourElement: ContourElement<PitchDurationScale>): NoteSpec => {
         const [ pitch, duration, scale ] = contourElement as number[]
 
-        if (pitch === -1) {
+        if (pitch === STANDARD_PITCH_INDEX_INDICATING_REST) {
             return {
                 durationSpec: {
                     scalar: to.Scalar(duration),
